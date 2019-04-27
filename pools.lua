fc56d1b02ecfa21782a42f833f049f7fa6c63bc6
yarypert <yarypert@student.42.fr>
2019-04-27 17:01:52 +0200

    correctif pools.lua

--- pools.lua
+++ pools.lua
@@ -2,42 +2,43 @@ wishes =
 {
     money = 
     {
-      dollars = "Dollars" --ex: i want XXX Dollars
-      lottery = "to win the lottery"
+      dollars = "Dollars", --ex: i want XXX Dollars
+      lottery = "to win the lottery",
     }
     love = 
     {
-      wife = "to find a loving wife"
-      husband = "to find a loving husband"
-      kids = "to have kids" --ex: i want to have kids // i want my sister to have kids
+      wife = "to find a loving wife",
+      husband = "to find a loving husband",
+      kids = "to have kids", --ex: i want to have kids // i want my sister to have kids
     }
     power =
     {
-      job = "to get my dream job";
-      mayor = "to be elected mayor";
-      president = "to be elected president";
-      death = "to die" --ex: i want XXX to die
+      job = "to get my dream job",
+      mayor = "to be elected mayor",
+      president = "to be elected president",
+      death = "to die", --ex: i want XXX to die
     }
+}
 
 prices =
 {
     money =
     {
-      house = ""
-      car = ""
-      bank = ""
+      house = "",
+      car = "",
+      bank = "",
     }
     love =
     {
-      wife = ""
-      husband = ""
-      kids = ""
-      dog = ""
-      cat = ""
+      wife = "",
+      husband = "",
+      kids = "",
+      dog = "",
+      cat = "",
     }
     power =
     {
-      boss = "" --ex : patron de sa boite
-      celebrity = ""
+      boss = "", --ex : patron de sa boite
+      celebrity = "",
     }
 }
