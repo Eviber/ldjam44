62d0c9cf73493be452de8d40e1f958a2cc2f17df
yarypert <yarypert@student.42.fr>
2019-04-27 16:55:26 +0200

    price and wishes

---
+++ pools.lua
@@ -0,0 +1,43 @@
+wishes = 
+{
+    money = 
+    {
+      dollars = "Dollars" --ex: i want XXX Dollars
+      lottery = "to win the lottery"
+    }
+    love = 
+    {
+      wife = "to find a loving wife"
+      husband = "to find a loving husband"
+      kids = "to have kids" --ex: i want to have kids // i want my sister to have kids
+    }
+    power =
+    {
+      job = "to get my dream job";
+      mayor = "to be elected mayor";
+      president = "to be elected president";
+      death = "to die" --ex: i want XXX to die
+    }
+
+prices =
+{
+    money =
+    {
+      house = ""
+      car = ""
+      bank = ""
+    }
+    love =
+    {
+      wife = ""
+      husband = ""
+      kids = ""
+      dog = ""
+      cat = ""
+    }
+    power =
+    {
+      boss = "" --ex : patron de sa boite
+      celebrity = ""
+    }
+}
