85d62a32c20965c2306f98d5df2d66989dd02a7d
Eviber <youv.didj@gmail.com>
2019-04-28 18:06:25 +0200

    u got that

--- human.lua
+++ human.lua
@@ -13,20 +13,24 @@ local function getwish()
 	return rdtb(rdtb(wishesPool))
 end
 
+--[[
+local function sortitems(items)
+	local function s(a,b)
+	end
+	table.sort(items, s)
+end
+]]
+
 local function getItems()
 	local items = {}
 
 	for i = 1, rd(20) do
-		id = rd(#pricesPool)
-		if items[id] == nil then
-			items[id] = {
-				n = 1,
-				i = pricesPool[id],
-			}
-		else
-			items[id].n = items[id].n + 1
-		end
+		items[i] = {
+			i = pricesPool[rd(#pricesPool)],
+			check = false,
+		}
 	end
+	sortitems(items)
 	for i = 1, #pricesPool do
 		if items[i] then
 			print(items[i].i.str, items[i].n)
@@ -43,7 +47,6 @@ function Human:create(appearance)
 	hooman.name = gibName()
 	print("Name: " .. hooman.name)
 	hooman.items = getItems()
-
 	hooman.appearance = appearance
 	hooman.toRemove = toRemove
 	return hooman
