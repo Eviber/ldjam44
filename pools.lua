c5d291e7cf7cc25fad62b030d85592bd621fe0cc
Eclahn <lgdtheo@gmail.com>
2019-04-28 13:50:00 +0200

    commit ta mère

--- pools.lua
+++ pools.lua
@@ -6,13 +6,13 @@ wishesPool =
         job = {"to get my dream job", 5, "possessions"},
         mayor = {"to be elected mayor", 5, "possessions"},
         president = {"to be elected president", 5, "possessions"},
-    }
+    },
     relations = {
         death = {"to die", 5, "relations"}, --ex: i want XXX to die
         wife = {"to find a loving wife", 5, "relations"},
         husband = {"to find a loving husband", 5, "relations"},
         kids = {"to have kids", 5, "relations"}, --ex: i want to have kids // i want my sister to have kids
-    }
+    },
     ego = {
         pianist = {"to become the best pianist", 5, "ego"},
     }
